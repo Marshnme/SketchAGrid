9d1267843b804696cbd24493a7a0b13ec85f2f2f
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-04-27 20:41:39 -0400

    correct amount of rows and columns. Still cant get divs to stay in canvas container completely

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ let input = 16;
 
 let canvasGridCounter = 0
 // creating the amount of divs needed for canvas based on input
-for(let i =0;i<input; i++){
-    // let gridDiv=document.createElement("div")
-    // canvas.appendChild(gridDiv)
-    // gridDiv.classList.add("grid-border")
+for(let i = 0;i<input; i++){
+    let gridRow=document.createElement("div")
+    gridRow.classList.add("canvas-row")
+    canvas.appendChild(gridRow)
+    // gridRow.classList.add("grid-border")
     // gridDiv.id = `canvas-grid-${i}`
     for(let j = 0; j<input; j++){
         let gridDiv=document.createElement("div")
-        // gridDiv.style.width=`${50 / input}%`;
-        // gridDiv.style.height=`${50 / input}%`;
+        gridDiv.style.width=`${500 / input}px`;
+        gridDiv.style.height=`${500 / input}px`;
         gridDiv.classList.add("grid-border")
         gridDiv.id = `canvas-grid-${canvasGridCounter}`
-        canvas.appendChild(gridDiv)
+        gridRow.appendChild(gridDiv)
         canvasGridCounter++
     }
 }
