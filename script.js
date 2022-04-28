23ad7562da797c29250bf439cf2e933e8434998e
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-04-28 19:47:05 -0400

    grid size slider is working. Removes old grid and recreates with new size

--- script.js
+++ script.js
@@ -1,52 +1,72 @@
 // selecting canvas parent to append div children based on input
-const canvas = document.querySelector(".canvas")
-let canvasSize = document.querySelector(".canvas-size-input")
+const canvasContainer = document.querySelector(".canvas")
 
-let input = 16;
-
-let canvasGridCounter = 0
-// creating the amount of rows and divs needed for canvas based on input
-for(let i = 0;i<input; i++){
-    let gridRow=document.createElement("div")
-    gridRow.classList.add("canvas-row")
-    canvas.appendChild(gridRow)
-    for(let j = 0; j<input; j++){
-        let gridDiv=document.createElement("div")
-        gridDiv.style.width=`${500 / input}px`;
-        gridDiv.style.height=`${500 / input}px`;
-        gridDiv.classList.add("grid-border")
-        gridDiv.id = `canvas-grid-${canvasGridCounter}`
-        gridRow.appendChild(gridDiv)
-        canvasGridCounter++
-    }
+let slider = document.getElementById("myRange");
+let inputDisplay = document.querySelector(".current-size")
+inputDisplay.innerHTML = slider.value;
+slider.oninput = function() {
+    inputDisplay.innerHTML = this.value;
 }
 
+slider.addEventListener("mouseup", remake)
+
+
+
+// creating the amount of rows and divs needed for canvas based on input
+function canvas(input){
+    let canvasGridCounter = 0
+    for(let i = 0;i<input; i++){
+        let gridRow=document.createElement("div")
+        gridRow.classList.add("canvas-row")
+        canvasContainer.appendChild(gridRow)
+        for(let j = 0; j<input; j++){
+            let gridDiv=document.createElement("div")
+            gridDiv.style.width=`${500 / input}px`;
+            gridDiv.style.height=`${500 / input}px`;
+            gridDiv.classList.add("grid-border")
+            gridDiv.id = `canvas-grid-${canvasGridCounter}`
+            gridRow.appendChild(gridDiv)
+            canvasGridCounter++
+        }
+    }
+    clickAndDrag()
+    clearBoard()
+}
+
+canvas(parseInt(slider.value))
+
+function remake(e){
+    document.querySelectorAll(".canvas-row").forEach(e => e.remove());
+    canvas(parseInt(slider.value))
+}
 
 
 // Selecting every canvas grid. Adding mouse click event into hover event listener to each canvas grid item to turn black on click and drag. Listening to window mouseup to remove hover event
-let canvasDivs = document.querySelectorAll(".grid-border");
-canvasDivs.forEach(div=>{
-    div.addEventListener("mousedown",changeColor)
-})
-
-
-function changeColor(e){
-    e.preventDefault();
-
+function clickAndDrag(){
+    let canvasDivs = document.querySelectorAll(".grid-border");
     canvasDivs.forEach(div=>{
-        div.addEventListener("mouseover",changeColor)
+        div.addEventListener("mousedown",changeColor)
     })
+
+
+    function changeColor(e){
+        e.preventDefault();
+
+        canvasDivs.forEach(div=>{
+         div.addEventListener("mouseover",changeColor)
+        })
     
-    let currentCanvasDiv = document.querySelector(`#${e.target.id}`);
-    currentCanvasDiv.style.backgroundColor="black"
+        let currentCanvasDiv = document.querySelector(`#${e.target.id}`);
+        currentCanvasDiv.style.backgroundColor="black";
+    }
+
+    window.addEventListener('mouseup', function(e){
+        canvasDivs.forEach(div=>{
+            div.removeEventListener("mouseover",changeColor)
+        })
+    })
+
 }
-
-window.addEventListener('mouseup', function(e){
-    canvasDivs.forEach(div=>{
-        div.removeEventListener("mouseover",changeColor)
-    })
-    })
-
 
 
 // Clear button functionality
@@ -54,6 +74,7 @@ let clearButton = document.querySelector(".clear-button")
 clearButton.addEventListener("mousedown",clearBoard)
 
 function clearBoard(){
+    let canvasDivs = document.querySelectorAll(".grid-border");
     canvasDivs.forEach(div=>{
         div.style.backgroundColor = "white";
     })
